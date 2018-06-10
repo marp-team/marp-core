4c9e6b0829f2229b5441a2b7510817fc738b02ce
Yuki Hattori <yukihattori1116@gmail.com>
2018-06-11 01:32:56 +0900

    Add gaia theme

--- src/marp.ts
+++ src/marp.ts
@@ -1,6 +1,7 @@
 /* tslint:disable: import-name */
 import { Marpit } from '@marp-team/marpit'
 import defaultTheme from '../themes/default.scss'
+import gaiaTheme from '../themes/gaia.scss'
 
 export class Marp extends Marpit {
   themeSet: any
@@ -9,5 +10,8 @@ export class Marp extends Marpit {
     super(...args)
 
     this.themeSet.default = this.themeSet.add(defaultTheme)
+    this.themeSet.add(gaiaTheme)
   }
 }
+
+export default Marp
---
+++ themes/gaia.scss.d.ts
@@ -0,0 +1,2 @@
+declare const theme: string
+export default theme
