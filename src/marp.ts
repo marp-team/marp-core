efef5f335ebef29b242a19ccc3586be09464d18a
Yuki Hattori <yukihattori1116@gmail.com>
2018-06-15 18:49:03 +0900

    Add Marp basic features and tests

--- src/marp.ts
+++ src/marp.ts
@@ -1,16 +1,54 @@
 /* tslint:disable: import-name */
 import { Marpit } from '@marp-team/marpit'
+import highlightjs from 'highlight.js'
+import markdownItEmoji from 'markdown-it-emoji'
 import defaultTheme from '../themes/default.scss'
 import gaiaTheme from '../themes/gaia.scss'
 
 export class Marp extends Marpit {
   themeSet: any
+  markdown: any
 
-  constructor(...args: any[]) {
-    super(...args)
+  constructor(opts: object = {}) {
+    super({
+      markdown: [
+        'commonmark',
+        {
+          breaks: true,
+          highlight: (code: string, lang: string) =>
+            this.highlighter(code, lang),
+          linkify: true,
+        },
+      ],
+      ...opts,
+    })
 
+    // Enable table
+    this.markdown.enable(['table', 'linkify'])
+
+    // Add themes
     this.themeSet.default = this.themeSet.add(defaultTheme)
     this.themeSet.add(gaiaTheme)
+  }
+
+  applyMarkdownItPlugins(md: any = this.markdown) {
+    super.applyMarkdownItPlugins(md)
+
+    // Emoji shorthand
+    md.use(markdownItEmoji, { shortcuts: {} })
+    md.renderer.rules.emoji = (token, idx) =>
+      `<span data-marpit-emoji>${token[idx].content}</span>`
+  }
+
+  highlighter(code: string, lang: string) {
+    if (lang) {
+      if (lang === 'text' || lang === 'plain') return ''
+      if (highlightjs.getLanguage(lang)) {
+        return highlightjs.highlight(lang, code, true).value
+      }
+    }
+
+    return highlightjs.highlightAuto(code).value
   }
 }
 
--- test/marp.ts
+++ test/marp.ts
@@ -1,8 +1,38 @@
 import { Marpit } from '@marp-team/marpit'
+import cheerio from 'cheerio'
 import { Marp } from '../src/marp'
 
 describe('Marp', () => {
-  it('extends Marpit', () => expect(new Marp()).toBeInstanceOf(Marpit))
+  const marp = (): Marp => new Marp()
+
+  it('extends Marpit', () => expect(marp()).toBeInstanceOf(Marpit))
+
+  describe('markdown property', () => {
+    it('renders breaks as <br> element', () => {
+      const $ = cheerio.load(marp().markdown.render('hard\nbreak'))
+      expect($('br').length).toBe(1)
+    })
+
+    it('has enabled table syntax', () => {
+      const $ = cheerio.load(marp().markdown.render('|a|b|\n|-|-|\n|c|d|'))
+      expect($('table > thead > tr > th').length).toBe(2)
+      expect($('table > tbody > tr > td').length).toBe(2)
+    })
+
+    it('converts URL to hyperlink', () => {
+      const address = 'https://www.google.com/'
+      const $ = cheerio.load(marp().markdown.render(address))
+      expect($(`a[href="${address}"]`).text()).toBe(address)
+    })
+
+    it('converts emoji shorthand to unicode emoji', () => {
+      const $ = cheerio.load(
+        marp().markdown.render('# emoji:heart:\n\n## emoji❤️')
+      )
+      expect($('h1').html()).toBe($('h2').html())
+      expect($('h1 > span[data-marpit-emoji]').length).toBe(1)
+    })
+  })
 
   describe('themeSet property', () => {
     const { themeSet } = new Marp()
@@ -10,6 +40,52 @@ describe('Marp', () => {
     it('has default theme', () => {
       expect(themeSet.default).toBeTruthy()
       expect(themeSet.default).toBe(themeSet.get('default'))
+    })
+  })
+
+  describe('#highlighter', () => {
+    describe('when fence is rendered without lang', () => {
+      const $ = cheerio.load(marp().markdown.render('```\n# test\n```'))
+
+      it('highlights code automatically', () =>
+        expect($('code > [class^="hljs-"]').length).toBeGreaterThan(0))
+    })
+
+    describe('when fence is rendered with specified lang', () => {
+      const $ = cheerio.load(marp().markdown.render('```markdown\n# test\n```'))
+
+      it('highlights code with specified lang', () => {
+        expect($('code.language-markdown').length).toBe(1)
+        expect($('code > .hljs-section').length).toBe(1)
+      })
+    })
+
+    // Plain rendering
+    ;['text', 'plain'].forEach(lang => {
+      describe(`when fence is rendered with ${lang} lang`, () => {
+        const $ = cheerio.load(
+          marp().markdown.render(`\`\`\`${lang}\n# test\n\`\`\``)
+        )
+
+        it('disables highlight', () =>
+          expect($('code > [class^="hljs-"]').length).toBe(0))
+      })
+    })
+
+    describe('with overriden #highlighter', () => {
+      const instance = marp()
+
+      instance.highlighter = (code, lang) => {
+        expect(code.trim()).toBe('test')
+        expect(lang).toBe('markdown')
+
+        return '<b class="customized">customized</b>'
+      }
+
+      const $ = cheerio.load(instance.markdown.render('```markdown\ntest\n```'))
+
+      it('highlights with custom highlighter', () =>
+        expect($('code > .customized').length).toBe(1))
     })
   })
 })
