3e99f0938a0e165dc6a8ef10be48cee7aebd400e
Yuki Hattori <yukihattori1116@gmail.com>
2018-08-05 06:00:58 +0900

    Support latest KaTeX with math option

---
+++ src/markdown/math.ts
@@ -0,0 +1,43 @@
+import katex from 'katex'
+import markdownItKatex from 'markdown-it-katex'
+import katexMinCss from 'katex/dist/katex.min.css'
+
+export function markdownItPlugin(md, katexOptions: object) {
+  const opts = (mergeOpts = {}) => ({
+    ...katexOptions,
+    ...mergeOpts,
+    throwOnError: true,
+  })
+
+  // Parse math syntax by using markdown-it-katex
+  md.use(markdownItKatex)
+
+  // Swap renderer to use the latest KaTeX
+  md.renderer.rules.math_inline = (tokens, idx) => {
+    const { content } = tokens[idx]
+    try {
+      return katex.renderToString(content, opts({ displayMode: false }))
+    } catch (e) {
+      console.warn(e)
+      return content
+    }
+  }
+
+  md.renderer.rules.math_block = (tokens, idx) => {
+    const { content } = tokens[idx]
+    try {
+      return `<p>${katex.renderToString(
+        content,
+        opts({ displayMode: true })
+      )}</p>`
+    } catch (e) {
+      console.warn(e)
+      return `<p>${content}</p>`
+    }
+  }
+}
+
+export function css() {
+  // TODO: Manipulate with PostCSS to change URL of KaTeX fonts
+  return katexMinCss
+}
--- src/marp.ts
+++ src/marp.ts
@@ -1,15 +1,19 @@
 /* tslint:disable: import-name */
-import { Marpit, MarpitOptions } from '@marp-team/marpit'
+import { Marpit, MarpitOptions, ThemeSetPackOptions } from '@marp-team/marpit'
 import highlightjs from 'highlight.js'
 import markdownItEmoji from 'markdown-it-emoji'
+import { markdownItPlugin as mathMD, css as mathCSS } from './markdown/math'
 import defaultTheme from '../themes/default.scss'
 import gaiaTheme from '../themes/gaia.scss'
 
 export interface MarpOptions extends MarpitOptions {
   html?: boolean
+  math?: boolean | object
 }
 
 export class Marp extends Marpit {
+  options!: MarpOptions
+
   constructor(opts: MarpOptions = {}) {
     super({
       markdown: [
@@ -22,6 +26,7 @@ export class Marp extends Marpit {
           linkify: true,
         },
       ],
+      math: true,
       ...opts,
     } as MarpitOptions)
 
@@ -40,6 +45,9 @@ export class Marp extends Marpit {
     md.use(markdownItEmoji, { shortcuts: {} })
     md.renderer.rules.emoji = (token, idx) =>
       `<span data-marpit-emoji>${token[idx].content}</span>`
+
+    // Math typesetting
+    if (this.options.math) md.use(mathMD)
   }
 
   highlighter(code: string, lang: string): string {
@@ -49,6 +57,17 @@ export class Marp extends Marpit {
         : ''
     }
     return highlightjs.highlightAuto(code).value
+  }
+
+  protected themeSetPackOptions(): ThemeSetPackOptions {
+    const base = { ...super.themeSetPackOptions() }
+
+    if (this.options.math) {
+      // Add KaTeX css
+      base.before = `${mathCSS()}\n${base.before || ''}`
+    }
+
+    return base
   }
 }
 
