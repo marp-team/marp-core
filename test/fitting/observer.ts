b4b0e68996a77b26015b7cd49bd4df57963776b3
Yuki Hattori <yukihattori1116@gmail.com>
2018-08-27 22:03:26 +0900

    Add test case of fitting observer about when rendering code block

--- test/fitting/observer.ts
+++ test/fitting/observer.ts
@@ -4,6 +4,12 @@ import Marp from '../../src/marp'
 import fittingObserver from '../../src/fitting/observer'
 
 describe('Fitting observer', () => {
+  const setContentSize = (content: HTMLElement, width, height) =>
+    Object.defineProperties(content, {
+      scrollWidth: { configurable: true, get: () => width },
+      scrollHeight: { configurable: true, get: () => height },
+    })
+
   it('calls window.requestAnimationFrame with myself', () => {
     const spy = jest.spyOn(window, 'requestAnimationFrame')
 
@@ -20,12 +26,6 @@ describe('Fitting observer', () => {
     let foreignObj: SVGForeignObjectElement
     let content: HTMLSpanElement
 
-    const setContentSize = (width, height) =>
-      Object.defineProperties(content, {
-        scrollWidth: { configurable: true, get: () => width },
-        scrollHeight: { configurable: true, get: () => height },
-      })
-
     beforeEach(() => {
       document.body.innerHTML = new Marp().render('# <!-- fit --> fitting').html
 
@@ -33,7 +33,7 @@ describe('Fitting observer', () => {
       foreignObj = svg.querySelector('foreignObject')
       content = foreignObj.querySelector('span[data-marp-fitting-svg-content]')
 
-      setContentSize(100, 200)
+      setContentSize(content, 100, 200)
     })
 
     it('updates SVG attributes into the content size', () => {
@@ -54,7 +54,7 @@ describe('Fitting observer', () => {
       fittingObserver()
       expect(svg.classList.contains('__reflow__')).toBe(true) // no change
 
-      setContentSize(200, 300)
+      setContentSize(content, 200, 300)
       fittingObserver()
       expect(foreignObj.getAttribute('width')).toBe('200')
       expect(foreignObj.getAttribute('height')).toBe('300')
@@ -63,7 +63,7 @@ describe('Fitting observer', () => {
     })
 
     it('keeps minimum svg size 1x1 even if content size is empty', () => {
-      setContentSize(0, 0)
+      setContentSize(content, 0, 0)
       fittingObserver()
 
       expect(foreignObj.getAttribute('width')).toBe('1')
@@ -90,16 +90,60 @@ describe('Fitting observer', () => {
     })
   })
 
-  // TODO: Add test case about when rendering code block
   context('when the auto-scalable code block is rendered', () => {
     let svg: SVGSVGElement
     let pre: HTMLPreElement
+    let content: HTMLSpanElement
 
     beforeEach(() => {
       document.body.innerHTML = new Marp().render('```\nauto-scalble\n```').html
 
       svg = document.querySelector('svg[data-marp-fitting-code]')
       pre = document.querySelector('section pre')
+      content = svg.querySelector('span[data-marp-fitting-svg-content]')
+
+      setContentSize(content, 200, 100)
+    })
+
+    const setPreWidth = clientWidth =>
+      Object.defineProperty(pre, 'clientWidth', {
+        configurable: true,
+        get: () => clientWidth,
+      })
+
+    it("restricts min width to <pre> element's width without padding", () => {
+      const computed = jest.spyOn(window, 'getComputedStyle')
+
+      try {
+        computed.mockImplementation(() => ({
+          paddingLeft: 0,
+          paddingRight: 0,
+          getPropertyValue: () => undefined,
+        }))
+
+        // pre width > svg width
+        setPreWidth(300)
+        fittingObserver()
+        expect(svg.getAttribute('viewBox')).toBe('0 0 300 100')
+
+        // svg width > pre width
+        setPreWidth(100)
+        fittingObserver()
+        expect(svg.getAttribute('viewBox')).toBe('0 0 200 100')
+
+        // Consider padding
+        computed.mockImplementation(() => ({
+          paddingLeft: '50px',
+          paddingRight: '70px',
+          getPropertyValue: () => undefined,
+        }))
+
+        setPreWidth(300) // 300 - 50 - 70 = 180px
+        fittingObserver()
+        expect(svg.getAttribute('viewBox')).toBe('0 0 200 100')
+      } finally {
+        computed.mockRestore()
+      }
     })
   })
 })
