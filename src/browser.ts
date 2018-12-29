89c73b93c86cdab2b9877cb81bb98eeb6d3dd0c4
Yuki Hattori <yukihattori1116@gmail.com>
2018-12-30 06:40:43 +0900

    Apply Marpit SVG polyfill for WebKit in Marp.ready()

--- src/browser.ts
+++ src/browser.ts
@@ -1,5 +1,12 @@
+import { polyfills } from '@marp-team/marpit-svg-polyfill'
 import fittingObserver from './fitting/observer'
 
 export default function browser(observe = true): void {
-  fittingObserver(observe)
+  const observer = () => {
+    for (const polyfill of polyfills()) polyfill()
+    fittingObserver()
+
+    if (observe) window.requestAnimationFrame(observer)
+  }
+  observer()
 }
--- test/browser.ts
+++ test/browser.ts
@@ -1,11 +1,40 @@
+/** @jest-environment jsdom */
 import browser from '../src/browser'
+import context from './_helpers/context'
 import fittingObserver from '../src/fitting/observer'
+
+const polyfill = jest.fn()
+
+jest.mock('@marp-team/marpit-svg-polyfill', () => ({
+  polyfills: () => [polyfill],
+}))
 
 jest.mock('../src/fitting/observer')
 
+beforeEach(() => jest.clearAllMocks())
+afterEach(() => jest.restoreAllMocks())
+
 describe('Browser observers', () => {
-  it('calls observers', () => {
+  it('executes observers for polyfill and fitting', () => {
+    const spy = jest.spyOn(window, 'requestAnimationFrame')
+
     browser()
-    expect(fittingObserver).toHaveBeenCalled()
+    expect(spy).toHaveBeenCalledTimes(1)
+    expect(polyfill).toHaveBeenCalledTimes(1)
+    expect(fittingObserver).toHaveBeenCalledTimes(1)
+
+    spy.mock.calls[0][0]()
+    expect(spy).toHaveBeenCalledTimes(2)
+    expect(polyfill).toHaveBeenCalledTimes(2)
+    expect(fittingObserver).toHaveBeenCalledTimes(2)
+  })
+
+  context('with observe argument is false', () => {
+    it('does not call window.requestAnimationFrame', () => {
+      const spy = jest.spyOn(window, 'requestAnimationFrame')
+
+      browser(false)
+      expect(spy).not.toHaveBeenCalled()
+    })
   })
 })
