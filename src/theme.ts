db885fcb64a25a4a4f30e9a32613bf369af2ba9e
Yuki Hattori <yukihattori1116@gmail.com>
2019-04-06 01:40:59 +0900

    Use nested prop support of ThemeSet#getThemeProp

--- src/theme.ts
+++ src/theme.ts
@@ -4,6 +4,5 @@ export function getThemeMeta(marp: Marp, meta: string): string | undefined {
   const { lastGlobalDirectives } = marp as any
   const theme = lastGlobalDirectives ? lastGlobalDirectives.theme : undefined
 
-  // FIXME: Follow imported meta
-  return marp.themeSet.getThemeProp(theme, ['meta', meta])
+  return marp.themeSet.getThemeProp(theme, `meta.${meta}`)
 }
