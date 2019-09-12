5e61edd6d7750adf6c86f20bde48c562204633b3
Yuki Hattori <yuki.hattori@speee.jp>
2019-09-12 10:36:27 +0900

    Optimize style of default theme on build time

---
+++ scripts/postcss-optimize-default-theme.js
@@ -0,0 +1,20 @@
+import postcss from 'postcss'
+
+const defaultThemeMatcher = /@theme +default/
+
+const plugin = postcss.plugin('postcss-optimize-default-theme', () => css => {
+  if (!defaultThemeMatcher.test(css.source.input.css)) return
+
+  // Remove rules for .markdown-body selector
+  css.walkRules(rule => {
+    const ss = rule.selectors.filter(s => !s.startsWith('.markdown-body'))
+
+    if (ss.length > 0) {
+      rule.selectors = ss
+    } else {
+      rule.remove()
+    }
+  })
+})
+
+export default plugin
