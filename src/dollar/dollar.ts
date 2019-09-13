c2c5dfdcc44bde66a88377d0f2c0b9414d9ad306
Yuki Hattori <yuki.hattori@speee.jp>
2019-09-13 18:48:09 +0900

    Fix dollar prefix plugin to support size directive

--- src/dollar/dollar.ts
+++ src/dollar/dollar.ts
@@ -5,7 +5,16 @@ import { Marp } from '../marp'
 export const markdown = marpitPlugin(({ marpit: marp }: { marpit: Marp }) => {
   if (!marp.options.dollarPrefixForGlobalDirectives) return
 
+  const customGlobalDirectives = Object.keys(marp.customDirectives.global)
+
+  // Marpit built-in directives
   marp.customDirectives.global.$theme = v => ({ theme: v })
   marp.customDirectives.global.$style = v => ({ style: v })
   marp.customDirectives.global.$headingDivider = v => ({ headingDivider: v })
+
+  // Custom global directives (e.g. `$size`)
+  for (const d of customGlobalDirectives) {
+    marp.customDirectives.global[`$${d}`] = v =>
+      marp.customDirectives.global[d](v)
+  }
 })
