86883a44cdb171eb301451c2f0f2bdb7dd658223
Yuki Hattori <yukihattori1116@gmail.com>
2022-05-21 17:15:08 +0900

    Migrate Jest config

--- test/_transformers/sass.js
+++ test/_transformers/sass.js
@@ -2,8 +2,9 @@ const { renderSync } = require('sass')
 const { createImporter } = require('sass-extended-importer')
 
 module.exports = {
-  process: (_, file) =>
-    `module.exports = ${JSON.stringify(
+  process: (_, file) => ({
+    code: `module.exports = ${JSON.stringify(
       renderSync({ file, importer: createImporter() }).css.toString()
     )};`,
+  }),
 }
