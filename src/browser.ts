55decb135239700685e1a4ad29e551836cfa196d
Yuki Hattori <yukihattori1116@gmail.com>
2022-06-05 00:02:24 +0900

    Update browser interface to add an interface for updating DOMs

--- src/browser.ts
+++ src/browser.ts
@@ -4,24 +4,43 @@ export { observer }
 
 const marpCoreBrowserScript = Symbol()
 
-export const browser = (target: ParentNode = document): (() => void) => {
+export interface MarpCoreBrowser {
+  /** Clean-up observer for applying polyfills. */
+  (): void
+
+  /** Clean-up observer for applying polyfills. */
+  cleanup: () => void
+
+  /**
+   * Update DOMs to apply custom elements for Marp Core. It should call whenever
+   * rendered DOMs rendered by Marp.
+   *
+   * It has exactly same meaning to call `browser()` with the same arguments
+   * again.
+   */
+  update: () => MarpCoreBrowser
+}
+
+export const browser = (target: ParentNode = document): MarpCoreBrowser => {
   if (typeof window === 'undefined') {
     throw new Error(
       "Marp Core's browser script is valid only in browser context."
     )
   }
 
+  applyCustomElements(target) // Should call in every update of Marp rendering
+
   if (target[marpCoreBrowserScript]) return target[marpCoreBrowserScript]
 
-  // ---
-
-  applyCustomElements(target)
-
   const cleanupObserver = observer({ target })
-  const cleanup = () => {
+  const cleanupFn = () => {
     cleanupObserver()
     delete target[marpCoreBrowserScript]
   }
+  const cleanup = Object.assign(cleanupFn, {
+    cleanup: cleanupFn,
+    update: () => browser(target),
+  })
 
   Object.defineProperty(target, marpCoreBrowserScript, {
     configurable: true,
