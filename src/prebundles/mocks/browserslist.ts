bdf4b465e5b56bbb2e915e45c814783b356a80c3
Yuki Hattori <yukihattori1116@gmail.com>
2022-09-09 01:35:10 +0900

    Disable collecting coverage from browserslist mock

--- src/prebundles/mocks/browserslist.ts
+++ src/prebundles/mocks/browserslist.ts
@@ -1,4 +1,6 @@
 // postcss-minify-params is depending on browserslist to detect whether using
 // IE. Marp does never use this detection so we will mock the module and return
 // empty array. You can see the setting for pre-bundling in rollup.config.js.
+
+/* istanbul ignore next */
 export default () => []
