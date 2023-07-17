61af4ccb0fac311f209b9043a2ee62ed972c909b
Yuki Hattori <yukihattori1116@gmail.com>
2023-07-18 00:17:53 +0900

    Remove @marp-team/marp-cli from devDependencies

---
+++ marp.config.mjs
@@ -0,0 +1,15 @@
+import path from 'node:path'
+import { Marp } from './lib/marp.js'
+
+export default {
+  engine: Marp,
+  server: true,
+  inputDir: path.join(
+    path.dirname(new URL(import.meta.url).pathname),
+    './sandbox'
+  ),
+  html: true,
+  options: {
+    minifyCSS: false,
+  },
+}
